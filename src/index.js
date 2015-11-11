b60289c165083d78aa91e6bcbe2609f1f3ee1dd9
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-11 22:17:55 +0200

    Pass action object to know which action was triggered

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const notify = (events) => ({ dispatch }) => next => action => {
   const returnValue = next(action);
   events.forEach( event => {
     if (event.catch.indexOf(action.type) !== -1) {
-      if (event.dispatch instanceof Function) dispatch(event.dispatch());
+      if (event.dispatch instanceof Function) dispatch(event.dispatch(action));
       else if (event.dispatch instanceof Array) {
-        event.dispatch.forEach( fn => setTimeout(() => { dispatch(fn()) }, 0) );
+        event.dispatch.forEach( fn => setTimeout(() => { dispatch(fn(action)) }, 0) );
       }
       else throw new Error('Expected dispatch value to be a function or an array of functions.');
     }
