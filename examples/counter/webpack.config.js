08bab1e3c5d927f549172cb2d89b299ae645a242
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-15 17:34:46 +0200

    Install react-toolbox for counter example

--- examples/counter/webpack.config.js
+++ examples/counter/webpack.config.js
@@ -1,7 +1,10 @@
 var path = require('path');
 var webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  context: __dirname,
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
@@ -13,16 +16,28 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
+    new ExtractTextPlugin('react-toolbox.css', { allChunks: true }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
+  resolve: {
+    extensions: ['', '.jsx', '.scss', '.js', '.json'],
+    modulesDirectories: [
+      'node_modules',
+      path.resolve(__dirname, './node_modules')
+    ]
+  },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /(\.js|\.jsx)$/,
       loaders: ['babel'],
       exclude: /node_modules/,
       include: __dirname
+    }, {
+      test: /(\.scss|\.css)$/,
+      loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
     }]
-  }
+  },
+  postcss: [autoprefixer]
 };
